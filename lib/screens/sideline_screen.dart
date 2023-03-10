9fbf2166cecccb4cc6da36bf4a81bce1021d0ad5
Elijah Truitt <elijahtr423@gmail.com>
2023-03-10 02:28:07 -0600

    WWWWW

--- lib/screens/sideline_screen.dart
+++ lib/screens/sideline_screen.dart
@@ -32,48 +32,216 @@ class _SidelineScreenState extends State<SidelineScreen> {
     return Container(
       child: Column(
         children: [
-          Row(
-            children: [
-              const Spacer(), // TODO(elijahtruitt): switch to padding.
-              Image.asset('assets/images/QR.png'),
-              const Spacer(),
-              ElevatedButton(
-                style: ButtonStyle(
-                    foregroundColor:
-                        MaterialStateProperty.all<Color>(unselectedColor)),
-                onPressed: () {},
-                child: const Text("Join the sidelines"),
+          Padding(
+            padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 10),
+            child: Row(
+              children: [
+                Image.asset('assets/images/QR.png'),
+                const Spacer(),
+                ElevatedButton(
+                  style: ButtonStyle(
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(unselectedColor)),
+                  onPressed: () {},
+                  child: const Text("Join the sidelines"),
+                ),
+                const Spacer(),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => const ChatScreen()));
+                  },
+                  child: Image.asset('assets/images/chat_bubble.png'),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: MediaQuery.of(context).size.height / 2.26,
+              child: ListView(
+                children: [
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    elevation: 10,
+                    shadowColor: Theme.of(context).secondaryHeaderColor,
+                    child: ListTile(
+                      trailing: const Padding(
+                        padding: EdgeInsets.only(right: 5),
+                      ),
+                      tileColor: Theme.of(context).focusColor,
+                      contentPadding: const EdgeInsets.all(10),
+                      leading: const CircleAvatar(
+                        radius: 30,
+                      ),
+                      title: const Text("Nick Bosa",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold)),
+                      subtitle: const Text("DE - San Francisco 49ers",
+                          style: TextStyle(color: Colors.white, fontSize: 15)),
+                    ),
+                  ),
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    elevation: 10,
+                    shadowColor: Theme.of(context).secondaryHeaderColor,
+                    child: ListTile(
+                      trailing: const Padding(
+                        padding: EdgeInsets.only(right: 5),
+                      ),
+                      tileColor: Theme.of(context).focusColor,
+                      contentPadding: const EdgeInsets.all(10),
+                      leading: const CircleAvatar(
+                        radius: 30,
+                      ),
+                      title: const Text("Nick Bosa",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold)),
+                      subtitle: const Text("DE - San Francisco 49ers",
+                          style: TextStyle(color: Colors.white, fontSize: 15)),
+                    ),
+                  ),
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    elevation: 10,
+                    shadowColor: Theme.of(context).secondaryHeaderColor,
+                    child: ListTile(
+                      trailing: const Padding(
+                        padding: EdgeInsets.only(right: 5),
+                      ),
+                      tileColor: Theme.of(context).focusColor,
+                      contentPadding: const EdgeInsets.all(10),
+                      leading: const CircleAvatar(
+                        radius: 30,
+                      ),
+                      title: const Text("Nick Bosa",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold)),
+                      subtitle: const Text("DE - San Francisco 49ers",
+                          style: TextStyle(color: Colors.white, fontSize: 15)),
+                    ),
+                  ),
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    elevation: 10,
+                    shadowColor: Theme.of(context).secondaryHeaderColor,
+                    child: ListTile(
+                      trailing: const Padding(
+                        padding: EdgeInsets.only(right: 5),
+                      ),
+                      tileColor: Theme.of(context).focusColor,
+                      contentPadding: const EdgeInsets.all(10),
+                      leading: const CircleAvatar(
+                        radius: 30,
+                      ),
+                      title: const Text("Nick Bosa",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold)),
+                      subtitle: const Text("DE - San Francisco 49ers",
+                          style: TextStyle(color: Colors.white, fontSize: 15)),
+                    ),
+                  ),
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    elevation: 10,
+                    shadowColor: Theme.of(context).secondaryHeaderColor,
+                    child: ListTile(
+                      trailing: const Padding(
+                        padding: EdgeInsets.only(right: 5),
+                      ),
+                      tileColor: Theme.of(context).focusColor,
+                      contentPadding: const EdgeInsets.all(10),
+                      leading: const CircleAvatar(
+                        radius: 30,
+                      ),
+                      title: const Text("Nick Bosa",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold)),
+                      subtitle: const Text("DE - San Francisco 49ers",
+                          style: TextStyle(color: Colors.white, fontSize: 15)),
+                    ),
+                  ),
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    elevation: 10,
+                    shadowColor: Theme.of(context).secondaryHeaderColor,
+                    child: ListTile(
+                      trailing: const Padding(
+                        padding: EdgeInsets.only(right: 5),
+                      ),
+                      tileColor: Theme.of(context).focusColor,
+                      contentPadding: const EdgeInsets.all(10),
+                      leading: const CircleAvatar(
+                        radius: 30,
+                      ),
+                      title: const Text("Nick Bosa",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold)),
+                      subtitle: const Text("DE - San Francisco 49ers",
+                          style: TextStyle(color: Colors.white, fontSize: 15)),
+                    ),
+                  ),
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    elevation: 10,
+                    shadowColor: Theme.of(context).secondaryHeaderColor,
+                    child: ListTile(
+                      trailing: const Padding(
+                        padding: EdgeInsets.only(right: 5),
+                      ),
+                      tileColor: Theme.of(context).focusColor,
+                      contentPadding: const EdgeInsets.all(10),
+                      leading: const CircleAvatar(
+                        radius: 30,
+                      ),
+                      title: const Text("Nick Bosa",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold)),
+                      subtitle: const Text("DE - San Francisco 49ers",
+                          style: TextStyle(color: Colors.white, fontSize: 15)),
+                    ),
+                  ),
+                ],
               ),
-              const Spacer(),
-              GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => const ChatScreen()));
-                },
-                child: Image.asset('assets/images/chat_bubble.png'),
-              ),
-              const Spacer()
-            ],
+            ),
           ),
           Column(children: [
-            const Text(
-              "Insights",
-              style: TextStyle(color: Colors.white),
-            ),
-            const Text(
-              "Statistics",
-              style: TextStyle(color: Colors.white),
-            ),
-            const Text(
-              "Insight",
-              style: TextStyle(color: Colors.white),
-            ),
+            const Padding(padding: EdgeInsets.all(10)),
             Image.asset('assets/images/Voice_Ai.png'),
             const Text(
               "Ask me about why Quarterbacks slide...",
               style:
                   TextStyle(fontStyle: FontStyle.italic, color: Colors.white),
-            )
+            ),
           ]),
         ],
       ),
