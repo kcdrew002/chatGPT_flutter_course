87bf1e2a62a35908acb31839d566804be87deec8
GrantComm <grantcommodore24@gmail.com>
2023-03-09 21:19:39 -0600

    Works

---
+++ lib/services/assets_manager.dart
@@ -0,0 +1,8 @@
+class AssetsManager {
+  static String imagePath = "assets/images";
+  static String userImage = "$imagePath/chatUser.png";
+  static String botImage = "$imagePath/resizedLogo.jpg";
+  static String openaiLogo = "$imagePath/openai_logo.jpg";
+  static String nflLogo = "$imagePath/logo3.png";
+  static String chatter = "$imagePath/chatUser.png";
+}
